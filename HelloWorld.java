fa07b84cb02e9b9d70325c0d390170f9fb067360
sivaramakrishnasirangu <105477354+sivaramakrishnasirangu@users.noreply.github.com>
2022-05-13 12:13:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
-public class HelloWorld
-{
-public static void main(String args[])
-{
- system.out.println("HelloWorld");
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
 }
-)
